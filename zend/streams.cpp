fe010747f04622491ee08487ecc7be5d462ab87d
Bartłomiej Krukowski <bkrukowski@olx.pl>
2016-07-07 15:28:18 +0200

    Support for clang++ compiler

--- zend/streams.cpp
+++ zend/streams.cpp
@@ -17,21 +17,21 @@ namespace Php {
  *  Some static buffers for writing data
  *  @var StreamBuf
  */
-static thread_local StreamBuf bufOut        (0);
-static thread_local StreamBuf bufError      (E_ERROR);
-static thread_local StreamBuf bufWarning    (E_WARNING);
-static thread_local StreamBuf bufNotice     (E_NOTICE);
-static thread_local StreamBuf bufDeprecated (E_DEPRECATED);
+static StreamBuf bufOut        (0);
+static StreamBuf bufError      (E_ERROR);
+static StreamBuf bufWarning    (E_WARNING);
+static StreamBuf bufNotice     (E_NOTICE);
+static StreamBuf bufDeprecated (E_DEPRECATED);
 
 /**
  *  Create the actual steams
  *  @var std::ostream
  */
-thread_local std::ostream out               (&bufOut);
-thread_local std::ostream error             (&bufError);
-thread_local std::ostream warning           (&bufWarning);
-thread_local std::ostream notice            (&bufNotice);
-thread_local std::ostream deprecated        (&bufDeprecated);
+std::ostream out               (&bufOut);
+std::ostream error             (&bufError);
+std::ostream warning           (&bufWarning);
+std::ostream notice            (&bufNotice);
+std::ostream deprecated        (&bufDeprecated);
 
 /**
  *  End namespace
